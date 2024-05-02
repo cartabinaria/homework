edcd3e2d0f9822288efbc6e37b9d4ea16aaa6adb
Alice Benatti <alice17bee@gmail.com>
2024-05-02 12:19:50 +0200

    new svelte

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [svelte()],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-	css: {
-		postcss: {
-			plugins: [tailwindcss, autoprefixer,]
-		}
 	}
 });
